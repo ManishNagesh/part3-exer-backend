6fa791e3c9dec212bb5756f4826097fb2428961e
ManishNagesh <dev.manishn@gmail.com>
2024-01-25 13:22:44 +0530

    part3-19-22

--- index.js
+++ index.js
@@ -12,109 +12,107 @@ app.use(cors())
 app.use(express.static('dist'))
 
 morgan.token('body', req => {
-    return JSON.stringify(req.body)
+  return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :response-time ms :body'))
 
-let persons = []
-
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(person => {
-        response.json(person)
-      })
+  Person.find({}).then(person => {
+    response.json(person)
+  })
 })
 
 app.get('/info',async(request, response) => {
-    try {
-        // Make a GET request to the /api/persons endpoint
-        const personsResponse = await axios.get(`http://localhost:${process.env.PORT}/api/persons`);
+  try {
+    // Make a GET request to the /api/persons endpoint
+    const personsResponse = await axios.get(`http://localhost:${process.env.PORT}/api/persons`)
 
-        const length = personsResponse.data.length;
-        const time = new Date();
-        
-        response.send(`<p>Phonebook has info for ${length} people</p>
-        <p>${time}</p>`);
-    } catch (error) {
-        console.error(error);
-        response.status(500).send('Internal Server Error');
-    }
+    const length = personsResponse.data.length
+    const time = new Date()
+
+    response.send(`<p>Phonebook has info for ${length} people</p>
+        <p>${time}</p>`)
+  } catch (error) {
+    console.error(error)
+    response.status(500).send('Internal Server Error')
+  }
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
+  Person.findById(request.params.id)
     .then(person => response.send(person))
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const {name, number} = request.body
+  const { name, number } = request.body
 
-    Person.findByIdAndUpdate(
-            request.params.id,
-            {name, number}, 
-            { new: true, runValidators: true, context: 'query'}
-        )
-      .then(updatedPerson => {
-        response.json(updatedPerson)
-      })
-      .catch(error => next(error))
-  })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
 
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    
-    // const nameFind = persons.find(person => person.name === body.name)
-    if(!body.name){
-        return response.status(400).json({
-            error: 'please include name'
-        })
-    }
-    // } else if(!body.number){
-    //     return response.status(400).json({
-    //         error: 'please include number'
-    //     })
-    // }
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    })
+  const body = request.body
 
-    
-    // Using save method triggers Mongoose schema validation
-    person.save()
-        .then(savedPerson => {
-            response.json(savedPerson);
-        })
-        .catch(error => next(error))
+  // const nameFind = persons.find(person => person.name === body.name)
+  if(!body.name){
+    return response.status(400).json({
+      error: 'please include name'
+    })
+  }
+  // } else if(!body.number){
+  //     return response.status(400).json({
+  //         error: 'please include number'
+  //     })
+  // }
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+
+  // Using save method triggers Mongoose schema validation
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
 }
-  
+
 // handler of requests with result to errors
 app.use(errorHandler)
 
 
 const PORT = process.env.PORT
-app.listen(PORT, ()=> {
-    console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
